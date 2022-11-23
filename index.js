950b21c702f15451fdb83c6e39a53ac37178c240
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-24 01:31:40 +0600

    added mongodb user and pass, created function run for server connection with mongodb

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
-
+// App creation and port
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Middle Wares
 app.use(cors());
 app.use(express.json());
+
+// MongoDB Credential
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsvgbta.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
+
+
 
 app.get('/', (req, res) => {
     res.send("Time Craft server is running!")
