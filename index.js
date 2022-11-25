f907c21b26d9027372f9f0f085d75c445384469f
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-25 19:26:37 +0600

    added verifyJWT middelware, along with user verification route /jwt, decoded the token send from jwt route, while checking the user via verifyJWT, implemented successfully inside get alluser api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 // App creation and port
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 // Middle Wares
 app.use(cors());
 app.use(express.json());
+
+function verifyJwt(req, res, next) {
+    // console.log('token inside verifyJwt', req.headers.authorization)
+    const authHeader = req.headers.authorization
+    if(!authHeader){
+        res.status(401).send("Unauthorized access")
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token , process.env.ACCESS_TOKEN, function(error, decoded){
+        if(error){
+            res.status(403).send({message : 'forbidden access'})
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 // MongoDB Credential
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsvgbta.mongodb.net/?retryWrites=true&w=majority`;
@@ -25,12 +42,27 @@ async function run() {
             const userData = await usersCollection.insertOne(user)
             res.send(userData)
         })
-        app.get('/users', async (req, res) => {
+        app.get('/users', verifyJwt, async (req, res) => {
+            const email = req.query.email
+            const decodedEmail = req.decoded.email
+            if(email !== decodedEmail){
+                return res.status(403).send({message : 'forbidden access'})
+            }
             const query = {};
             const userData = await usersCollection.find(query).toArray();
             res.send(userData)
         })
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        })
 
 
     }
