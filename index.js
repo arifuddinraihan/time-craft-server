e09bc83844cc856201804d5f7a9fb5bb4cef3cf8
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-11-26 17:18:36 +0600

    added condition in all users acces for admin only

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -19,6 +19,7 @@ function verifyJwt(req, res, next) {
         res.status(401).send("Unauthorized access")
     }
     const token = authHeader.split(' ')[1];
+    // console.log(token)
     jwt.verify(token, process.env.ACCESS_TOKEN, function (error, decoded) {
         if (error) {
             res.status(403).send({ message: 'forbidden access' })
@@ -44,14 +45,22 @@ async function run() {
 
         app.post('/newsLetterEmails', async (req, res) => {
             const email = req.body;
-            console.log(email)
+            // console.log(email)
             const newsLetterEmail = await newsLetterCollection.insertOne(email)
             res.send(newsLetterEmail)
         })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
-            const userData = await usersCollection.insertOne(user)
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: user?.name,
+                    imageURL: user?.imageURL,
+                },
+            };
+            const userData = await usersCollection.updateOne(user, updateDoc, options)
+            // console.log(userData)
             res.send(userData)
         })
 
@@ -101,12 +110,36 @@ async function run() {
             res.send(userData)
         })
 
+        // Admin making an Admin from usersCollection only buyer can e be an Admin
+        app.put('/users/admin/:id', verifyJwt, async (req, res) => {
+            const decodedEmail = req.decoded.email
+            const query = { email: decodedEmail }
+            const user = await usersCollection.findOne(query)
+            if (user.admin === "yes") {
+                return res.status(403).send({ message: 'forbidden access, your not an Admin' })
+            }
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    admin: "yes"
+                }
+            }
+            // console.log(id)
+            // console.log(filter)
+            // const query = { role: "Buyer" };
+            const result = await usersCollection.updateOne(filter, updateDoc, options)
+            res.send(result)
+        })
+
+
         app.post('/allProducts', async (req, res) => {
             const productDetails = req.body;
             const newPostedProduct = await allProductsCollection.insertOne(productDetails)
             res.send(newPostedProduct);
         })
-        app.get('/allProducts',verifyJwt, async (req, res) => {
+        app.get('/allProducts', verifyJwt, async (req, res) => {
             const email = req.query.email
             const decodedEmail = req.decoded.email
             if (email !== decodedEmail) {
@@ -116,14 +149,16 @@ async function run() {
             const getAllProducts = await allProductsCollection.find(query).toArray()
             res.send(getAllProducts);
         })
-        app.get('/allProducts', verifyJwt, async (req, res) => {
+        app.get('/allProducts/seller', verifyJwt, async (req, res) => {
             const email = req.query.email
+            // console.log(email)
             const decodedEmail = req.decoded.email
             if (email !== decodedEmail) {
                 return res.status(403).send({ message: 'forbidden access' })
             }
-            const query = { sellerEmail: email };
-            const userData = await usersCollection.find(query).toArray();
+            const query = { sellerEmail: decodedEmail };
+            const userData = await allProductsCollection.find(query).toArray();
+            // console.log(userData)
             res.send(userData)
         })
 
